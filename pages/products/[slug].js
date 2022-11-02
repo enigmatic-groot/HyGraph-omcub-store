ca18b992309577950baf2ed81949e13249f8accb
Raqib Abdullah <raqibjs93@gmail.com>
2022-11-02 23:40:53 +0600

    sassa

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -5,20 +5,19 @@ import getProductBySlug from '@/lib/get-product-slug'
 import getPageData from '@/lib/get-page-data'
 import ProductPageUI from '@/components/product-page-ui'
 import SEO from '@/components/seo'
-import Layout from '@/components/Layout'
 import Container from '@/components/Container'
 import Section from '@/components/Section'
 
 function ProductPage({ product }) {
   return (
-    <Layout>
+    <>
       <SEO title={product.name} {...product} />
       <Section>
         <Container>
           <ProductPageUI product={product} />
         </Container>
       </Section>
-    </Layout>
+    </>
   )
 }
 
